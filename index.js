8a20377d4360d0127a026527e90e9ab031c0fb74
ananthujp <31985570+ananthujp@users.noreply.github.com>
2023-02-06 22:43:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
-//module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = app;
+//module.exports.handler = serverless(app);
